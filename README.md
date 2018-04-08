e69734aadb59f4ee2b7f7445ecc31520d909b338
raildex1 <raildex1@users.noreply.github.com>
2018-04-08 23:53:33 +1000

    Added setup script. Modified readme.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# re_demo_setup
+This is a repo just for the RE seminar - the install script will install 
+(heh) everything you need.
+
+Important Notes:
+
+You MUST use Ubuntu 16.04 for this script to work. 
+Kali Linux will NOT work.
+
+Instructions:
+The one liner to use to install is:
+
+git clone https://github.com/raildex1/re_demo_setup && 
+bash re_demo_setup/re_demo_setup.sh
+
+Or if you already cloned the repo and are in the re_demo_setup 
+directory:
+
+bash re_demo_setup.sh
---
+++ re_demo_setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo Installing everything needed for Ubuntu 16.04 for the Reverse 
+Engineering seminar...
+echo
+
+sudo apt update
+sudo apt install unzip gdb git -y
+git clone https://github.com/pwndbg/pwndbg
+cd pwndbg
+
+echo
+echo
+echo Make sure to type Y if prompted and press enter...
+echo
+echo
+
+bash setup.sh
+cd ..
+wget https://cdn.binary.ninja/installers/BinaryNinja-demo.zip
+unzip BinaryNinja-demo.zip
+rm -f BinaryNinja-demo.zip
+
+echo
+echo 
+echo Install Complete! If any errors occured please make a pull request 
+:)
+echo To start gdb (with pwndbg) type:
+echo gdb
+echo 
+echo To start BinaryNinja from your home directory type:
+echo ./binaryninja/binaryninja
+echo
