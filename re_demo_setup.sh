5d239c001fe48f4245e1c12472c33eb1cc30bc4d
raildex1 <raildex1@users.noreply.github.com>
2018-05-06 19:23:15 +1000

    Update re_demo_setup.sh
    Fixed open-vm-tools since 100% of people are doing this in a VM.

--- re_demo_setup.sh
+++ re_demo_setup.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 set -e
-echo "Installing everything needed for Ubuntu 16.04 for the RevEng seminar"
-echo
-
-sudo apt update
-sudo apt install unzip gdb git -y
-git clone https://github.com/pwndbg/pwndbg
-cd pwndbg
-
-echo
+echo "Installing everything needed for Ubuntu 16.04 LTS"
 echo
 echo "Make sure to type Y if prompted and press enter..."
 echo
 echo
+
+sudo add-apt-repository ppa:ci-train-ppa-service/3152
+sudo apt update
+sudo apt install unzip gdb git open-vm-tools open-vm-tools-desktop open-vm-tools-dkms -y
+git clone https://github.com/pwndbg/pwndbg
+cd pwndbg
 
 bash setup.sh
 cd ..
